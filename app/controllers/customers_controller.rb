7008cf35041f05aa07caff4a406219b83df66397
Sylpha777 <sylpha.ponrena@gmail.com>
2021-06-09 02:23:45 +0000

    customers show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
   
   before_action :require_customer_logged_in, only: [:show, :edit, :update]
+  before_action :correct_customer, only: [:show, :edit, :update]
   
   def show
   end
@@ -24,12 +25,26 @@ class CustomersController < ApplicationController
   end
 
   def update
+    if @customer.update(customer_params)
+      flash[:success] = '変更しました。'
+      redirect_to @customer
+    else
+      flash.now[:danger] = '変更に失敗しました。'
+      render :new
+    end
   end
   
   private
 
   def customer_params
     params.require(:customer).permit(:name, :address, :phone, :email, :password, :password_confirmation)
+  end
+  
+  def correct_customer
+    @customer = Customer.find(params[:id])
+    if @customer.id != session[:customer_id]
+      redirect_to root_url
+    end
   end
   
 end
